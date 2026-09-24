a81c13418c43c8cb0f6a7368968373526095300c
olga-pad <olgapadkovenka@gmail.com>
2026-09-24 22:27:25 +0200

    feat: implement Catch game

--- games.js
+++ games.js
@@ -1,2 +1,2 @@
 'use strict';
-document.write('<script src="./games/games.js?v=2"><\/script><script src="./games/find.js?v=2"><\/script><script src="./games/build-word.js?v=2"><\/script><script src="./games/missing-word.js?v=2"><\/script>');
+document.write('<script src="./games/games.js?v=2"><\/script><script src="./games/find.js?v=2"><\/script><script src="./games/build-word.js?v=2"><\/script><script src="./games/missing-word.js?v=2"><\/script><script src="./games/catch.js?v=1"><\/script>');
